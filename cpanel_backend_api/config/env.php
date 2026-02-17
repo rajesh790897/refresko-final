7b0db474acc56c8de2d5723b6bed8ecbd551ef99
ANkit Rajak <ankit12rajak@gmail.com>
2026-02-17 14:47:33 +0530

    no

--- cpanel_backend_api/config/env.php
+++ cpanel_backend_api/config/env.php
@@ -5,7 +5,7 @@ return [
         'host' => 'localhost',
         'port' => 3306,
         'name' => 'skfedbzl_refresko_prod',
-        'user' => 'skfedbzl_refresko_api',
+        'user' => 'skfedbzl_refapi_api',
         'pass' => 'refresko26',
         'charset' => 'utf8mb4',
     ],
